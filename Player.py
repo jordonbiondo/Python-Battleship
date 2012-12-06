2d9d9376d106638c9405b601c428f1f1c5359e8a
Jordon Biondo <biondoj@mail.gvsu.edu>
2012-12-05 19:27:03 -0500

    Ignore and player

---
+++ Player.py
@@ -0,0 +1,17 @@
+from Geometry2D import *
+
+class Player(object):
+    
+    def __init(self):
+        None
+
+        
+    def giveInstruction(self):
+        print "Enter position"
+        x = int(raw_input())
+        y = int(raw_input())
+        return Point(x, y)
+
+a = Player()
+
+print a.giveInstruction()
